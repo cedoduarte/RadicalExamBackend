28b5ed7bd76fe4f58e233e321cfa96d518c73183
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-07-27 16:43:12 -0700

    feat: Date checking for Banxico controller

--- RadicalExam/Controllers/BanxicoController.cs
+++ RadicalExam/Controllers/BanxicoController.cs
@@ -17,6 +17,13 @@ namespace RadicalExam.Controllers
         [HttpGet("exchange-rate/{startDate}/{endDate}/{token}")]
         public async Task<IActionResult> GetExchangeRate(string startDate, string endDate, string token)
         {
+            string format = "yyyy-MM-dd";
+            DateTime startDateTime = DateTime.ParseExact(startDate, format, null);
+            DateTime endDateTime = DateTime.ParseExact(endDate, format, null);
+            if (startDateTime > endDateTime)
+            {
+                return BadRequest("¡La fecha de inicio debe ser anterior a la fecha de fin!");
+            }
             _banxicoService.Token = token;
             return Ok(await _banxicoService.GetExchangeRate(startDate, endDate));
         }
